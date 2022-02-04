916e010c3963458d374847519c82612cc32b4f1d
Nikhil-69 <nikhil.mittal.ug20@nsut.ac.in>
2022-02-04 18:41:24 +0530

    Add node.js

---
+++ Nodejs/fileSystem/index.js
@@ -0,0 +1,16 @@
+// fs import file system functions so we can use read and write oprations
+const fs = require("fs");
+//path function helps us to give path
+const path = require("path");
+//path.join join path of file and __dirname gives path to the current directory
+const p1 = path.join(__dirname, "files/abc.txt");
+
+const data = "Wrinting the data using node.js";
+
+fs.writeFile(p1, data, (err) => {
+  if (err) console.log("error");
+});
+fs.readFile(p1, (err, data) => {
+  if (err) console.log(err);
+  else if (data) console.log(data.toString());
+});
---
+++ Nodejs/sharingContent/math.js
@@ -0,0 +1,9 @@
+const PI = 3.145205;
+const cube =n => n * n * n;
+const sqareRoot = n => Math.sqrt(n);
+
+
+module.exports = {
+    PI,
+    cube
+}
